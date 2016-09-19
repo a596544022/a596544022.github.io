78cf1176326422e5c33dbcde40783d7d6019322e
迷离 <596544022@qq.com>
2016-09-19 16:32:04 +0800

    11

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+	uglify = require('gulp-uglify'),
+    minifycss = require('gulp-minify-css'),
+	imagemin = require('gulp-imagemin'),
+	htmlmin = require('gulp-htmlmin');
+gulp.task('css', function() {
+  gulp.src('less/*.less').pipe(less()).pipe(gulp.dest('css'))
+});
+gulp.task('script', function() {
+    gulp.src('js2/*.js').pipe(uglify()).pipe(gulp.dest('js'))
+})
+gulp.task('minifycss', function() {
+	gulp.src('css/*.css').pipe(minifycss()).pipe(gulp.dest('css'));   
+});
+gulp.task('img', () =>
+    gulp.src('img/*').pipe(imagemin()).pipe(gulp.dest('img'))
+);
+gulp.task('img', function () {
+    gulp.src('img/*.{png,jpg,gif,ico}')
+        .pipe(imagemin({
+            optimizationLevel: 3, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+        .pipe(gulp.dest('disc/img'));
+});
+gulp.task('default', function() {
+  gulp.watch('less/*.less',['css']);
+  gulp.watch('css/*.css',['minifycss']);
+  gulp.watch('js2/*.js',['script']);
+});
